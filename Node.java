e8b4279a856202802daa2a899fc5bb55b3f78262
Angel1092 <angel.guerra@malad.us>
2026-03-11 14:15:47 -0700

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,12 @@
+public class Node {
+
+    Student data;
+    Node next;
+    Node previous;
+
+    public Node(Student data) {
+        this.data = data;
+        this.next = null;
+        this.previous = null;
+    }
+}
